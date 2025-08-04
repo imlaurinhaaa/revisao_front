d77f0ca2b380fa56004805a47cbca76c7ebbb9f1
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-08-03 22:33:52 -0300

    feat: finish welcome

---
+++ src/app/welcome/page.jsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './Welcome.module.css';
+
+export default function Welcome() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <Image
+          src="/image/log-semfundo.png"
+          alt="Logo Image"
+          width={300}
+          height={300}
+          className={styles.image}
+        />
+        <h1 className={styles.title}>Bem-Vindo (a) Cozy Loops!</h1>
+        <p className={styles.description}>
+          Na Cozy Loops, cada fio ganha vida em peças artesanais de crochê cheias de personalidade e carinho. Aqui, o aconchego se transforma em arte feita à mão, pronta para trazer conforto e estilo para o seu dia a dia. 
+        </p>
+        <Link href="/home" className={styles.button}>
+          Acessar
+        </Link>
+        </div>
+    </div>
+  );
+}
+
